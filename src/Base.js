5aea0066437e7712a4cb2d00a25a898f8b6e44ba
Tomohiro Suwa <neoen.gsn@gmail.com>
2017-01-20 19:01:07 +0900

    Introduce eslint

--- src/Base.js
+++ src/Base.js
@@ -2,36 +2,42 @@ import axios from 'axios';
 
 export default class Base {
   /**
-   * Create API Client class
-   * @param {Object} params
-   * @param {string} params.service - target service
-   * @param {string} params.userAgent - your User-Agent
+   * create API Client class
+   * @param {string} service target service
+   * @param {string} userAgent your User-Agent
+   * @public
    */
-  constructor(params) {
-    this.service = params.service;
-    this.userAgent = params.userAgent || 'niconico-search';
+  constructor({ service, userAgent }) {
+    this.service = service;
+    this.userAgent = userAgent || 'niconico-search';
     this.client = axios.create({
       baseURL: 'http://api.search.nicovideo.jp'
     });
   }
 
   /**
-   * Request
+   * request
    * @see http://search.nicovideo.jp/docs/api/search.html
-   * @param {string} search - search word
-   * @param {Object} params - other queries
+   * @param {string} search search word
+   * @param {Object} params other queries
+   * @public
    * @return {Promise.<Object, Error>}
    */
   search(search, params = {}) {
     const queryHash = Object.assign(
       { q: search, _context: this.userAgent },
-      this._default_params(),
+      this._defaultParams(),
       params
     );
     return this.client.get(`/api/v2/${this.service}/contents/search`, { params: queryHash });
   }
 
-  _default_params() {
+  /**
+   * default parameters
+   * @return {Object} default parameters
+   * @private
+   */
+  _defaultParams() {
     return {
       targets: 'title',
       fields: 'contentId',
